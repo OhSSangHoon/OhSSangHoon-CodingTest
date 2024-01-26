4d51cd423c10ce4d9dc1c716eb95c8151b9d35bd
tkdhun <114225559+OhSSangHoon@users.noreply.github.com>
2024-01-26 18:11:34 +0900

    Update CTP공국으로 이민 가자.cc

--- 백준/Bronze/12778. CTP공국으로 이민 가자/CTP공국으로 이민 가자.cc
+++ 백준/Bronze/12778. CTP공국으로 이민 가자/CTP공국으로 이민 가자.cc
@@ -2,25 +2,28 @@
 
 using namespace std;
 
+//1. 시험문제의 개수 N의 크기만큼 반복문 실행
+//2. 알파벳 또는 숫자의 개수 M, 알파벳을 숫자로 변경하는 C, 숫자를 알파벳으로 변경하는 N
+//3. M의 크기만큼 반복문 실행 s가 C라면 char c를 입력 후 int형으로 변경 후 출력
+//4. s가 N이라면 int c를 입력 후 char형으로 변경 후 출력
+
 int main(){
     int n;
     cin >> n;
-
-    int a = 1;
 
     for(int i =0; i < n; i++){
         int m = 0;
         char s;
 
         cin >> m >> s;
-
+        
         if(s == 'C'){
             for(int j = 0; j < m; j++){
                 char c;
                 cin >> c;
                 cout << (int)(c - 'A' + 1) << " ";
             }
-        }else if (s == 'N'){
+        }else if(s == 'N'){
             for(int j = 0; j < m; j++){
                 int c;
                 cin >> c;
