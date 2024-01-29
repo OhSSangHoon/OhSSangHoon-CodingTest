153976bb8a547bbf2bee315c7df8f82e558f3c10
tkdhun <114225559+OhSSangHoon@users.noreply.github.com>
2024-01-29 19:23:27 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n;
+    long long a,b;
+    long long triangle =0;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> a >> b;
+
+        triangle = a / b * a / b;
+
+        cout << triangle << endl;
+    }
+    return 0;
+}
