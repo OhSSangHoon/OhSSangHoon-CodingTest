eded4513f0b3188735bc9cfd1e5fd177aab0cb32
tkdhun <114225559+OhSSangHoon@users.noreply.github.com>
2024-10-22 17:27:08 +0900

    [level 0] Title: 정수 부분, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.js
@@ -0,0 +1,12 @@
+function solution(flo) {
+    var answer = 0;
+    
+    // Math.floor
+    // parseInt
+    // ~~연산자
+    // 소숫점을 떼는 용도로 자주 쓰이는 방법
+    
+    answer = ~~(flo);
+    
+    return answer;
+}
