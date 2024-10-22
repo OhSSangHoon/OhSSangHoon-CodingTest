6ef8e023b17db8d3f629ddd6328fd818311f72d4
tkdhun <114225559+OhSSangHoon@users.noreply.github.com>
2024-10-22 17:47:56 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -0,0 +1,15 @@
+function solution(num_list) {    
+    let temp;
+    
+    for(let i = 0; i < num_list.length; i++)
+        for(let j = 0; j < num_list.length; j++)
+            if(num_list[i] < num_list[j]){
+                temp = num_list[i];
+                num_list[i] = num_list[j];
+                num_list[j] = temp;
+            }
+    
+    
+    return num_list.slice(5);
+    
+}
