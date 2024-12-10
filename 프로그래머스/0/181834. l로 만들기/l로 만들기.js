ad2fb9242ff36d1c95b48717271cdd6c9f6778c7
tkdhun <114225559+OhSSangHoon@users.noreply.github.com>
2024-12-10 13:12:02 +0900

    [level 0] Title: l로 만들기, Time: 22.38 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,5 +1,7 @@
-//문자열 탐색하면서 i보다 작은 알파벳은 i로 변환한 뒤 대문자로 변환
+//문자열 탐색하면서 l보다 작은 알파벳은 l로 출력 l보다 크면 그대로 출력
 
 function solution(myString) {
+    //myString은 문자열이라서 map을 사용하지못함 그래서 배열로 변경한 뒤 map함수 사용
     return [...myString].map(char => (char < 'l' ? 'l' : char)).join('');
 }
+
